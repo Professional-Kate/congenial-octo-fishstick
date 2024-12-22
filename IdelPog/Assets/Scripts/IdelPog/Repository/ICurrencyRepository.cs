f9bf001e6e2c197b25e7dbc723b6f85d21cc0648
Professional-Kate <professionalkateg@gmail.com>
2024-12-22 01:22:05 +0000

    added docs

--- IdelPog/Assets/Scripts/IdelPog/Repository/ICurrencyRepository.cs
+++ IdelPog/Assets/Scripts/IdelPog/Repository/ICurrencyRepository.cs
@@ -1,22 +1,29 @@
-﻿using IdelPog.Model;
+﻿using System;
+using IdelPog.Model;
 using IdelPog.Structures;
 
 namespace IdelPog.Repository
 {
     /// <summary>
-    /// The purpose of this interface is to give access to Adding, Removing, and finally getting an object from an internal data structure
+    /// The purpose of this interface is to give access to Adding, Removing, and finally getting a <see cref="Currency"/> from an internal data structure
     /// </summary>
     public interface ICurrencyRepository
     {
-        // TODO: When tests are added update these doc comments with exactly what exceptions they can throw and any remarks of each method.
-        
         /// <summary>
         /// Adds a <see cref="Currency"/> model with a connected <see cref="CurrencyType"/> key into the Repository
         /// </summary>
         /// <param name="currency">The <see cref="Currency"/> model you want to add</param>
         /// <returns>if the operation was successful</returns>
+        /// <exception cref="ArgumentNullException">If the passed <see cref="Currency"/> model is null</exception>
+        /// <exception cref="ArgumentException">If the <see cref="Currency"/> model <see cref="CurrencyType"/> is <see cref="CurrencyType.NO_TYPE"/></exception>
+        /// <exception cref="ArgumentException">If the passed <see cref="Currency"/> model already exists in the Repository</exception>
         /// <remarks>
-        /// The passed <see cref="Currency"/> will be linked to the <see cref="CurrencyType"/> key in the Repository
+        /// Every implementation of this method should follow these rules :
+        /// <list type="bullet">
+        /// <item> You should throw an exception if the passed <see cref="Currency"/> model <see cref="CurrencyType"/> is <see cref="CurrencyType.NO_TYPE"/></item>
+        /// <item> The passed <see cref="Currency"/> will be linked to the <see cref="CurrencyType"/> key in the Repository </item>
+        /// <item> Always ensure that this method will throw an exception if the passed <see cref="Currency"/> model already exists in the Repository </item>
+        /// </list>
         /// </remarks>
         public bool Add(Currency currency);
 
@@ -25,6 +32,15 @@ namespace IdelPog.Repository
         /// </summary>
         /// <param name="currencyType">The <see cref="Currency"/> model you want to remove should have this <see cref="CurrencyType"/></param>
         /// <returns>if the operation was successful</returns>
+        /// <exception cref="ArgumentException">If the passed <see cref="CurrencyType"/> is <see cref="CurrencyType.NO_TYPE"/></exception>
+        /// <exception cref="ArgumentException">If the passed <see cref="CurrencyType"/> is not in the Repository</exception>
+        /// <remarks>
+        /// Every implementation of this method should follow these rules :
+        /// <list type="bullet">
+        /// <item> You should throw an exception if the passed <see cref="CurrencyType"/> is <see cref="CurrencyType.NO_TYPE"/></item>
+        /// <item> An exception should be thrown if the <see cref="Currency"/> is not found in the Repository</item>
+        /// </list>
+        /// </remarks>
         public bool Remove(CurrencyType currencyType);
 
         /// <summary>
