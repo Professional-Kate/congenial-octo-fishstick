331904bf62b4a9cc4bc23d90c6b804c992dbe117
Professional-Kate <professionalkateg@gmail.com>
2025-01-22 22:54:38 +0000

    Added Inventory. This is how we store items!

---
+++ IdelPog/Assets/Scripts/IdelPog/Repository/Inventory/IInventory.cs
@@ -0,0 +1,42 @@
+﻿using IdelPog.Structures.Item;
+
+namespace IdelPog.Repository
+{
+    /// <summary>
+    /// Using this interface you can adjust the amount of any <see cref="Item"/> in the Inventory. 
+    /// </summary>
+    /// <seealso cref="AddAmount"/>
+    /// <seealso cref="RemoveAmount"/>
+    /// <seealso cref="AddItem"/>
+    /// <seealso cref="Contains"/>
+    public interface IInventory
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="amount"></param>
+        public void AddAmount(InventoryID item, int amount);
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="amount"></param>
+        public void RemoveAmount(InventoryID item, int amount);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="startingAmount"></param>
+        public void AddItem(InventoryID item, int startingAmount);
+
+        /// <summary>
+        ///  
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Contains(InventoryID item);
+    }
+}
---
+++ IdelPog/Assets/Scripts/IdelPog/Repository/Inventory/Inventory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using IdelPog.Structures.Item;
+
+namespace IdelPog.Repository
+{
+    /// <summary>
+    /// The container class for all <see cref="Item"/>'s. See <see cref="IInventory"/> for documentation
+    /// </summary>
+    public sealed class Inventory : IInventory
+    {
+       private readonly Repository<InventoryID, Item> _inventory;
+
+        public void AddAmount(InventoryID item, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAmount(InventoryID item, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddItem(InventoryID item, int startingAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(InventoryID item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
