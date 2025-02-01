d02d2012c8f35809f20d63d2c9c5c294814a5530
Professional-Kate <professionalkateg@gmail.com>
2025-02-01 21:17:22 +0000

    Created new Integration directory and created the CurrencyFlowTest

---
+++ IdelPog/Assets/Scripts/Tests/Integration/CurrencyFlowTest.cs
@@ -0,0 +1,11 @@
+﻿using IdelPog.Controller;
+using NUnit.Framework;
+
+namespace Tests.Integration
+{
+    [TestFixture]
+    public class CurrencyFlowTest
+    {
+        private readonly ICurrencyController _currencyController = CurrencyController.GetInstance();
+    }
+}
