6178139b6be8e7e0d2b2595c23fb6192874c6c71
Professional-Kate <professionalkateg@gmail.com>
2025-03-06 22:16:55 +0000

    Added AssertPositive and a ThrowHandler

---
+++ IdelPog/Assets/Scripts/IdelPog/Validation/Assertions/AssertPositive.cs
@@ -0,0 +1,33 @@
+﻿using IdelPog.Validation.Handlers.Interfaces;
+
+namespace IdelPog.Validation
+{
+    public class AssertPositive : AssertionsHandler
+    {
+        public AssertPositive(IHandler handler) : base(handler) { } 
+        
+        /// <summary>
+        /// Asserts that the passed int, or int array, is zero or above
+        /// </summary>
+        /// <param name="numbers">The number you want to assert</param>
+        /// <exception cref="NegativeNumberException">WIll be thrown if the number is less than zero</exception>
+        public void AssertNumberIsPositive(params int[] numbers)
+        {
+            Handle(() =>
+            {
+                foreach (int number in numbers)
+                {
+                    AssertNumber(number);
+                }
+            });
+        }
+
+        private static void AssertNumber(int number)
+        {
+            if (number < 0)
+            {
+                throw new NegativeNumberException(number);
+            }
+        }
+    }
+}
---
+++ IdelPog/Assets/Scripts/IdelPog/Validation/Assertions/Handlers/ThrowHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using IdelPog.Validation.Handlers.Interfaces;
+
+namespace IdelPog.Validation.Handlers
+{
+    public class ThrowHandler : IHandler
+    {
+        public void Handle(Exception exception)
+        {
+            throw exception;
+        }
+    }
+}
