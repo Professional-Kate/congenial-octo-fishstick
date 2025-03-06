cbbbf5ae2799e197237edd4260e78a82a18ceb74
Professional-Kate <professionalkateg@gmail.com>
2025-03-06 22:16:37 +0000

    added assertion classes

---
+++ IdelPog/Assets/Scripts/IdelPog/Validation/Assertions/AssertionsHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using IdelPog.Validation.Handlers.Interfaces;
+
+namespace IdelPog.Validation
+{
+    public abstract class AssertionsHandler 
+    {
+        private readonly IHandler _handler;
+
+        protected AssertionsHandler(IHandler handler)
+        {
+            _handler = handler;
+        }
+
+        protected void Handle(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception exception)
+            {
+                _handler.Handle(exception);
+            }
+        }
+    }
+}
---
+++ IdelPog/Assets/Scripts/IdelPog/Validation/Assertions/Handlers/Interfaces/IHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IdelPog.Validation.Handlers.Interfaces
+{
+    public interface IHandler
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="exception"></param>
+        public void Handle(Exception exception);
+    }
+}
