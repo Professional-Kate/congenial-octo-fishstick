d48ece4f3f08f6a6a6ceb0b7c85900aebbcf705a
Professional-Kate <professionalkateg@gmail.com>
2025-03-15 21:35:36 +0000

    added CurrencyBuilder

---
+++ IdelPog/Assets/Scripts/IdelPog/Structures/Models/Builders/Currency/CurrencyBuilder.cs
@@ -0,0 +1,31 @@
+﻿using IdelPog.Model;
+using IdelPog.Structures.Enums;
+
+namespace IdelPog.Structures.Builders
+{
+    /// <inheritdoc cref="ICurrencyBuilder"/>
+    public class CurrencyBuilder : ICurrencyBuilder
+    {
+        private CurrencyType _currencyType { get; set; }
+        private int _amount { get; set; }
+
+        public ICurrencyBuilder CurrencyType(CurrencyType currencyType)
+        {
+            _currencyType = currencyType;
+
+            return this;
+        }
+
+        public ICurrencyBuilder Amount(int amount)
+        {
+            _amount = amount;
+
+            return this;
+        }
+
+        public Currency Build()
+        {
+            return new Currency(_currencyType, _amount);
+        }
+    }
+}
