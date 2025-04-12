871f8dce0a31de76e27f5000d99c04d713eadb46
Professional-Kate <professionalkateg@gmail.com>
2025-04-12 17:21:07 +0100

    Updated asserter classes to use primary constructors <33

--- IdelPog/Engine/Validation/Pipelines/LevelableAsserter.cs
+++ IdelPog/Engine/Validation/Pipelines/LevelableAsserter.cs
@@ -4,24 +4,14 @@ using IdelPog.Engine.Validation.Assertions;
 namespace IdelPog.Engine.Validation.Pipelines
 {
     /// <inheritdoc cref="ILevelableAsserter"/>
-    public class LevelableAsserter : ILevelableAsserter
+    public class LevelableAsserter(IAssertUnderMaxLevel assertUnderMaxLevel, IAssertNotNull assertNotNull, IAssertPositive assertPositive)
+        : ILevelableAsserter
     {
-        private readonly IAssertUnderMaxLevel _assertUnderMaxLevel;
-        private readonly IAssertNotNull _assertNotNull;
-        private readonly IAssertPositive _assertPositive;
-        
-        public LevelableAsserter(IAssertUnderMaxLevel assertUnderMaxLevel, IAssertNotNull assertNotNull, IAssertPositive assertPositive)
-        {
-            _assertUnderMaxLevel = assertUnderMaxLevel;
-            _assertNotNull = assertNotNull;
-            _assertPositive = assertPositive;
-        }
-        
         public void AssertLevelable(ILevelable levelable)
         {
-            _assertNotNull.AssertObjectNotNull(levelable);
-            _assertUnderMaxLevel.AssertLevelIsUnderMax(levelable);
-            _assertPositive.AssertNumberIsPositive(levelable.ExperiencePerAction);
+            assertNotNull.AssertObjectNotNull(levelable);
+            assertUnderMaxLevel.AssertLevelIsUnderMax(levelable);
+            assertPositive.AssertNumberIsPositive(levelable.ExperiencePerAction);
         }
     }
 }
