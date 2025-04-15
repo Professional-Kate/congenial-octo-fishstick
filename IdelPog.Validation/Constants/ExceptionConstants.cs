f7f5861445b0463ec9367576bcfe803ea135c9a8
Professional-Kate <professionalkateg@gmail.com>
2025-04-15 21:41:51 +0100

    made ExceptionConstants internal

--- IdelPog.Validation/Constants/ExceptionConstants.cs
+++ IdelPog.Validation/Constants/ExceptionConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace IdelPog.Validation.Constants
 {
-    public static class ExceptionConstants
+    internal static class ExceptionConstants
     {
         public const string NOT_FOUND_MESSAGE = "Error! The passed ID {0} was not found!";
         public const string MAX_LEVEL_MESSAGE = "Error! The passed Job {0} is at max level! ";
