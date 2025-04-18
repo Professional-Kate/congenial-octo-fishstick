8cbb08fb2d702539886e4af74ce98e9b89d1728c
Professional-Kate <professionalkateg@gmail.com>
2025-04-18 16:27:49 +0100

    Created new generic JsonSourceConverter. Removed the old Information and Job converters

--- IdelPog.ContentHydrator/Converters/IJsonConverter.cs
+++ IdelPog.ContentHydrator/Converters/IJsonConverter.cs
@@ -1,16 +1,16 @@
 ﻿namespace ContentHydrator.Converters
 {
     /// <summary>
-    /// converts strings into the specified DTO 
+    /// converts JSON strings into the specified DTO 
     /// </summary>
     /// <typeparam name="T">The type of the DTO</typeparam>
     public interface IJsonConverter<out T>
     {
         /// <summary>
-        /// Converts given strings into a new instance of <typeparamref name="T"/>
+        /// Converts given JSON strings into a new instance of <typeparamref name="T"/>
         /// </summary>
-        /// <param name="content">The string to convert</param>
+        /// <param name="jsonString">The JSON string to convert</param>
         /// <returns>A new instance of type <typeparamref name="T"/></returns>
-        public T Convert(Dictionary<string, object> content);
+        public T Convert(string jsonString);
     }
 }
---
+++ IdelPog.ContentHydrator/Converters/JsonSourceConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization.Metadata;
+
+namespace ContentHydrator.Converters
+{
+    public class JsonSourceConverter<T>(JsonTypeInfo<T> typeInfo) : IJsonConverter<T>
+    {
+        public T Convert(string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
