bd07bc091f6d67a2a239e4514d4dd74c84720cb2
Professional-Kate <professionalkateg@gmail.com>
2025-04-19 15:05:14 +0100

    Updated positive test to check better

--- IdelPog.ContentHydrator.Tests/Service/DirectoryConverterTest.cs
+++ IdelPog.ContentHydrator.Tests/Service/DirectoryConverterTest.cs
@@ -58,9 +58,16 @@ namespace ContentHydratorTests.Service
                 new() { TestString = "Two", TestInt = 2 }
             ];
 
-            IEnumerable<TestDTO> returnedObjects = _directoryConverter.ConvertDirectory(DIRECTORY_PATH);
+            List<TestDTO> returnedObjects = _directoryConverter.ConvertDirectory(DIRECTORY_PATH).ToList();
             
-            Assert.That(returnedObjects, Is.EquivalentTo(expected));
+            Assert.Multiple(() =>
+            {
+                Assert.That(returnedObjects, Is.EquivalentTo(expected));
+                Assert.That(returnedObjects, Is.EqualTo(expected));
+            });
+            
+            _jsonConverterMock.Verify(library => library.Convert(It.IsAny<JsonDocument>()), Times.Exactly(expected.Count));
+            _jsonReaderMock.Verify(library => library.Read(It.IsAny<string>()), Times.Exactly(expected.Count));
         }
         
         [Test]
