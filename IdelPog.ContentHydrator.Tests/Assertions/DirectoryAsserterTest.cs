8820fce108a08907c7f7944dc967b4901bede33e
Professional-Kate <professionalkateg@gmail.com>
2025-04-22 20:32:57 +0100

    added a few more tests for DirectoryAsserterTest

--- IdelPog.ContentHydrator.Tests/Assertions/DirectoryAsserterTest.cs
+++ IdelPog.ContentHydrator.Tests/Assertions/DirectoryAsserterTest.cs
@@ -1,7 +1,9 @@
 ﻿using ContentHydrator.Assertions;
 using ContentHydrator.Assertions.Pipelines;
+using ContentHydrator.Exceptions;
 using IdelPog.Validation.Assertions;
 using IdelPog.Validation.Assertions.Handlers;
+using Moq;
 
 namespace ContentHydratorTests.Assertions
 {
@@ -9,30 +11,41 @@ namespace ContentHydratorTests.Assertions
     public class DirectoryAsserterTest
     {
         private IDirectoryAsserter _directoryAsserter { get; set; }
+        private Mock<IHandler> _handlerMock { get; set; }
 
-        [OneTimeSetUp]
-        public void OneTimeSetUp()
+        [SetUp]
+        public void SetUp()
         {
-            IHandler throwHandler = new ThrowHandler();
-            _directoryAsserter = new DirectoryAsserter(new AssertDirectoryFound(throwHandler), new AssertDirectoryNotEmpty(throwHandler), new AssertNotNull(throwHandler));
+            _handlerMock = new Mock<IHandler>();
+            _directoryAsserter = new DirectoryAsserter(
+                new AssertDirectoryFound(_handlerMock.Object), new AssertDirectoryNotEmpty(_handlerMock.Object), new AssertNotNull(_handlerMock.Object));
+            
+            _handlerMock.Setup(library => library.Handle(It.IsAny<Exception>()))
+                .Callback<Exception>(ex => throw ex);
         }
 
         [Test]
         public void Positive_AssertDirectory_CorrectDirectory()
         {
             Assert.DoesNotThrow(() => _directoryAsserter.AssertDirectory("Resources"));
+            
+            _handlerMock.Verify(library => library.Handle(It.IsAny<Exception>()), Times.Never);
         }
 
         [Test]
         public void Negative_AssertDirectory_MissingDirectory()
         {
             Assert.Throws<DirectoryNotFoundException>(() => _directoryAsserter.AssertDirectory("hidden"));
+            
+            _handlerMock.Verify(library => library.Handle(It.IsAny<DirectoryNotFoundException>()), Times.Once);
         }
 
         [Test]
         public void Negative_AssertDirectory_NullDirectory()
         {
             Assert.Throws<ArgumentNullException>(() => _directoryAsserter.AssertDirectory(null!));
+            
+            _handlerMock.Verify(library => library.Handle(It.IsAny<ArgumentNullException>()), Times.Once);
         }
 
         [Test]
@@ -41,6 +54,24 @@ namespace ContentHydratorTests.Assertions
             string[] goodStrings = ["testing", "hello"];
             
             Assert.DoesNotThrow(() => _directoryAsserter.AssertFiles(goodStrings, "no"));
+            
+            _handlerMock.Verify(library => library.Handle(It.IsAny<Exception>()), Times.Never);
+        }
+
+        [Test]
+        public void Negative_AssertFiles_NullArray_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => _directoryAsserter.AssertFiles(null!, "no"));
+            
+            _handlerMock.Verify(library => library.Handle(It.IsAny<ArgumentNullException>()), Times.Once);
+        }
+
+        [Test]
+        public void Negative_AssertFiles_EmptyArray_Throws()
+        {
+            Assert.Throws<EmptyDirectoryException>(() => _directoryAsserter.AssertFiles([], "no"));
+            
+            _handlerMock.Verify(library => library.Handle(It.IsAny<EmptyDirectoryException>()), Times.Once);
         }
     }
 }
