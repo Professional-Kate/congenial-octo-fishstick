f44ba3918ef83271782477216cc3c3808475cfcd
Professional-Kate <professionalkateg@gmail.com>
2025-05-30 21:37:12 +0100

    Added docs for buffer. I don't want to add more right now please don't make me boss

--- IdelPog.Staging/Collection/IBuffer.cs
+++ IdelPog.Staging/Collection/IBuffer.cs
@@ -2,7 +2,15 @@
 {
     public interface IBuffer<in T>
     {
-        public void MarkReady();
+        /// <summary>
+        /// Assign the passed array of data into the internal collection of the Buffer
+        /// </summary>
+        /// <param name="source">The data you want in the Buffer</param>
         public void Assign(T[] source);
+        
+        /// <summary>
+        /// Mark this buffer ready for consuming. After marking ready you will no longer be able to <see cref="Assign"/>
+        /// </summary>
+        public void MarkReady();
     }
 }
