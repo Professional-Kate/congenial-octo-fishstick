a7c8dc21933434f4faad7180e595a67801ebe520
Professional-Kate <professionalkateg@gmail.com>
2025-06-01 22:27:15 +0100

    Added 4 new assertion classes. Unused currently

---
+++ IdelPog.ECS/Assertions/AssertArrayNotNull.cs
@@ -0,0 +1,20 @@
+﻿using IdelPog.ECS.Exceptions;
+using IdelPog.Validation.Assertions;
+using IdelPog.Validation.Assertions.Handlers;
+
+namespace IdelPog.ECS.Assertions
+{
+    public class AssertArrayNotNull(IHandler handler) : BaseAssertion<ComponentArrayNullException>(handler)
+    {
+        public void Handle(bool arrayNotNull)
+        {
+            Assert(() =>
+            {
+                if (arrayNotNull == false)
+                {
+                    throw new ComponentArrayNullException();
+                }
+            });
+        }
+    }
+}
---
+++ IdelPog.ECS/Assertions/AssertComponentFound.cs
@@ -0,0 +1,21 @@
+﻿using IdelPog.ECS.Component;
+using IdelPog.ECS.Exceptions;
+using IdelPog.Validation.Assertions;
+using IdelPog.Validation.Assertions.Handlers;
+
+namespace IdelPog.ECS.Assertions
+{
+    public class AssertComponentFound(IHandler handler) : BaseAssertion<ComponentNotFoundException>(handler)
+    {
+        public void Handle(bool componentWasFound, IComponent componentContext)
+        {
+            Assert(() =>
+            {
+                if (componentWasFound == false)
+                {
+                    throw new ComponentNotFoundException(componentContext);
+                }
+            });
+        }
+    }
+}
