0b906b251f61b83f18690cddcc38af410d1e2c94
Professional-Kate <professionalkateg@gmail.com>
2025-06-08 15:33:38 +0100

    Added AssetRepository

---
+++ IdelPog.Infrastructure/Repository/Asset/AssetRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace IdelPog.Infrastructure.Repository
+{
+    public class AssetRepository<TID, T> : IAssetRepository<TID, T> where TID : notnull
+    {
+        private readonly Dictionary<TID, T> _repository = new();
+        
+        public void Add(TID key, T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TID key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(TID key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(TID key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IdelPog.Infrastructure/Repository/Asset/IAssetRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace IdelPog.Infrastructure.Repository
+{
+    public interface IAssetRepository<in TID, T>
+    {
+        public void Add(TID key, T entity);
+        
+        public void Remove(TID key);
+        
+        public T Get(TID key);
+        
+        public bool Contains(TID key);
+    }
+}
