462ae5b13c703b8f3ea66ad18b67bd71231b22cc
meat <professionalkateg@gmail.com>
2025-06-27 20:49:16 +0100

    Moved CurrencyType into flows

---
+++ Backend/SimulationEngine/Flows/Currency/Types/CurrencyType.cs
@@ -0,0 +1,11 @@
+﻿namespace IdelPog.SimulationEngine.Flows.Currency
+{
+    /// <summary>
+    /// Every Currency in the game will require one of these tags
+    /// </summary>
+    public enum CurrencyType : byte
+    {
+        FOOD,
+        WOOD
+    }
+}
