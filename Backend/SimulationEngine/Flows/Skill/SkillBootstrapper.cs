04db96dd9a96a0cb2800ed83f84b964e56611375
meat <professionalkateg@gmail.com>
2025-06-29 00:14:21 +0100

    Created SkillBootstrapper

---
+++ Backend/SimulationEngine/Flows/Skill/SkillBootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using IdelPog.Messaging.Dispatch;
+
+namespace IdelPog.SimulationEngine.Flows.Skill
+{
+    public class SkillBootstrapper
+    {
+        public void Initialize(IBufferMessenger bufferMessenger, ICurrentSkillSetter currentSkillSetter)
+        {
+            ISkillController skillController = new SkillController(currentSkillSetter);
+            SkillChangeListener skillChangeListener = new(skillController);
+            
+            bufferMessenger.Subscribe(skillChangeListener);
+        }
+    }
+}
