b96ed176dd7920618e0b8266a7d332cd1e41bbd5
meat <professionalkateg@gmail.com>
2025-07-09 20:21:38 +0100

    Added AssertPositive to the CurrencyService.

--- Backend/SimulationEngine/Flows/Currency/CurrencyService.cs
+++ Backend/SimulationEngine/Flows/Currency/CurrencyService.cs
@@ -1,19 +1,34 @@
-﻿namespace IdelPog.SimulationEngine.Currency
+﻿using IdelPog.SimulationEngine.Currency.Assertions;
+using IdelPog.SimulationEngine.Currency.Commands;
+
+namespace IdelPog.SimulationEngine.Currency
 {
     /// <summary>
     /// See <see cref="ICurrencyService"/> for documentation.
     /// </summary>
     public class CurrencyService : ICurrencyService
     {
+        private readonly IAssertPositive _assertPositive;
+
+        public CurrencyService(IAssertPositive assertPositive)
+        {
+            _assertPositive = assertPositive;
+        }
+
         public void AddAmount(Currency currency, int amount)
         {
+            _assertPositive.AssertNumberIsPositive<CurrencyUpdate>(amount);
+            
             int newAmount = currency.Amount + amount;
             currency.SetAmount(newAmount);
         }
 
         public void RemoveAmount(Currency currency, int amount)
         {
+            _assertPositive.AssertNumberIsPositive<CurrencyUpdate>(amount);
+            
             int newAmount = currency.Amount - amount;
+            // TODO: assert newAmount is positive
             currency.SetAmount(newAmount);
         }
     }
