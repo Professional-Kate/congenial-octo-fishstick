777d76db285c1071dfab6aa5f58a5ce3aada783f
meat <professionalkateg@gmail.com>
2025-07-13 16:29:06 +0100

    Added switchSkill tests

--- Integration.Tests/SkillCommands/Switch/SwitchSkillTest.cs
+++ Integration.Tests/SkillCommands/Switch/SwitchSkillTest.cs
@@ -1,4 +1,5 @@
-﻿using IdelPog.SimulationEngine.Skill;
+﻿using IdelPog.Messaging.Buffer;
+using IdelPog.SimulationEngine.Skill;
 
 namespace Integration.Tests.SkillCommands.Switch
 {
@@ -9,19 +10,60 @@ namespace Integration.Tests.SkillCommands.Switch
         private ICurrentSkillSetter _currentSkillSetter;
         private ICurrentSkillProvider _currentSkillProvider;
 
+        private SkillChange _skillChange;
+
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
+            _skillChange = new SkillChange { SkillID = SkillID.FARMING };
+            
             CurrentSkillProvider currentSkillProvider = new();
             _currentSkillSetter = currentSkillProvider;
             _currentSkillProvider = currentSkillProvider;
-            SkillBootstrapper.Initialize(BufferMessenger, _currentSkillSetter);
         }
         
         [SetUp]
         public void Setup()
         {
+            SkillBootstrapper.Initialize(BufferMessenger, BufferManager, _currentSkillSetter);
+            
             _listener = new SkillChangeDTOListener();
+            ManagedSubscribe(_listener);
+        }
+
+        private void SendChangeSkillBuffer(SkillChange skillChange)
+        {
+            IBuffer<SkillChange> buffer = BufferManager.RequestBuffer<SkillChange>(new BufferRequest(1));
+            buffer.Assign([skillChange]);
+            buffer.MarkReady();
+        }
+
+        private void AssertListener(SkillChange skillChange)
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(_listener.WasCalled, Is.True);
+                Assert.That(_currentSkillProvider.GetCurrentSkill(), Is.EqualTo(skillChange.SkillID));
+                Assert.That(_listener.SkillChangeDTO.SkillID, Is.EqualTo(skillChange.SkillID));
+            });
+        }
+        
+        [Test]
+        public void Positive_SendChangeSkill_SwitchesSkill_DispatchesSkillChangeDTO()
+        {
+            Assert.DoesNotThrow(() => SendChangeSkillBuffer(_skillChange));
+            AssertListener(_skillChange);
+        }
+
+        [Test]
+        public void Positive_SendSequentialCommands_SwitchesSkill_DispatchesSkillChangeDTO()
+        {
+            Assert.DoesNotThrow(() => SendChangeSkillBuffer(_skillChange));
+            AssertListener(_skillChange);
+            
+            SkillChange secondSkillChange = new() { SkillID = SkillID.MINING };
+            Assert.DoesNotThrow(() => SendChangeSkillBuffer(secondSkillChange));
+            AssertListener(secondSkillChange);
         }
     }
 }
