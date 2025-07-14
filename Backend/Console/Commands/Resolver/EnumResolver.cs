3690da3bb9b92d5dee3ec478d5a08dbd63004525
meat <professionalkateg@gmail.com>
2025-07-14 20:25:24 +0100

    Removed ActionTypeResolver and CurrencyTypeResolver, in favour of a more generic EnumResolver

---
+++ Backend/Console/Commands/Resolver/EnumResolver.cs
@@ -0,0 +1,22 @@
+﻿using Console.Commands.Assertions;
+
+namespace Console.Commands.Resolver
+{
+    public class EnumResolver<TEnum> : IArgumentResolver<TEnum> where TEnum : struct
+    {
+        private readonly IAssertCanParseEnum _assertCanParse;
+
+        public EnumResolver(IAssertCanParseEnum assertCanParse)
+        {
+            _assertCanParse = assertCanParse;
+        }
+        
+        public TEnum Resolve(string argument)
+        {
+            bool successfulParse = Enum.TryParse(argument, ignoreCase: true, out TEnum result);
+            _assertCanParse.Handle(successfulParse, argument, nameof(TEnum));
+
+            return result;
+        }
+    }
+}
