29b543c1fbb5f4e3a542c6f705d6952b872ca187
meat <professionalkateg@gmail.com>
2025-07-14 20:20:48 +0100

    Added ActionTypeResolver

--- Backend/Console/Commands/Domains/CurrencyDomainResolver.cs
+++ Backend/Console/Commands/Domains/CurrencyDomainResolver.cs
@@ -1,6 +1,7 @@
 ﻿using Console.Commands.Resolver;
 using Console.Types;
 using IdelPog.Common.Enums;
+using IdelPog.SimulationEngine.Structures;
 
 namespace Console.Commands.Domains
 {
@@ -8,11 +9,13 @@ namespace Console.Commands.Domains
     {
         public CommandDomain HandledDomain { get; } = CommandDomain.CURRENCY;
         
+        private readonly IArgumentResolver<ActionType>  _actionTypeResolver;
         private readonly IArgumentResolver<CurrencyType> _currencyTypeResolver;
         private readonly IArgumentResolver<int> _intResolver;
 
-        public CurrencyDomainResolver(IArgumentResolver<CurrencyType> currencyTypeResolver,  IArgumentResolver<int> intResolver)
+        public CurrencyDomainResolver(IArgumentResolver<ActionType> actionTypeResolver, IArgumentResolver<CurrencyType> currencyTypeResolver,  IArgumentResolver<int> intResolver)
         {
+            _actionTypeResolver = actionTypeResolver;
             _currencyTypeResolver = currencyTypeResolver;
             _intResolver = intResolver;
         }
@@ -21,17 +24,17 @@ namespace Console.Commands.Domains
         {
             // TODO: verify args are length two <CurrencyType> <int>
             
-            string normalizedAction = action.ToLowerInvariant();
+            ActionType actionType = _actionTypeResolver.Resolve(action);
             CurrencyType currencyType = _currencyTypeResolver.Resolve(args[0]);
             int amount = _intResolver.Resolve(args[1]);
             
-            switch (normalizedAction)
+            switch (actionType)
             {
-                case "add": 
+                case ActionType.ADD: 
                     // TODO: verify args have add <int>
                     // TODO: assign CurrencyUpdate
                     break;
-                case "remove":
+                case ActionType.REMOVE:
                     // TODO: verify args have remove <int>
                     // TODO: assign CurrencyUpdate
                     break;
---
+++ Backend/Console/Commands/Resolver/ActionTypeResolver.cs
@@ -0,0 +1,23 @@
+﻿using Console.Commands.Assertions;
+using IdelPog.SimulationEngine.Structures;
+
+namespace Console.Commands.Resolver
+{
+    public class ActionTypeResolver : IArgumentResolver<ActionType>
+    {
+        private readonly IAssertCanParseEnum _assertCanParse;
+
+        public ActionTypeResolver(IAssertCanParseEnum assertCanParse)
+        {
+            _assertCanParse = assertCanParse;
+        }
+
+        public ActionType Resolve(string argument)
+        {
+            bool successfulParse = Enum.TryParse(argument, ignoreCase: true, out ActionType result);
+            _assertCanParse.Handle(successfulParse, argument, nameof(ActionType));
+
+            return result;
+        }
+    }
+}
