189bfaf68d44ad3bf99bcb7d6b87ddf7db8f2dc9
meat <professionalkateg@gmail.com>
2025-07-17 21:23:55 +0100

    Added AllowedDomainsEntity code

--- Backend/Console/Runtime/ECS/AllowedDomainsEntity.cs
+++ Backend/Console/Runtime/ECS/AllowedDomainsEntity.cs
@@ -1,4 +1,5 @@
-﻿using IdelPog.ECS;
+﻿using Console.Types;
+using IdelPog.ECS;
 using IdelPog.ECS.Component;
 using IdelPog.Validation.Assertions.Handlers;
 
@@ -6,10 +7,26 @@ namespace Console.Runtime.ECS
 {
     public record AllowedDomainsEntity : Entity
     {
-        public AllowedDomainsEntity(CommandDomainComponent[] allowedDomains) 
-            : base(new ComponentStore<CommandDomainComponent>(allowedDomains, new ThrowHandler()))
+        private readonly ComponentStore<CommandDomainComponent>  _componentStore;
+        
+        public AllowedDomainsEntity(CommandDomainComponent[] allowedDomains) : base(new ComponentStore<CommandDomainComponent>(allowedDomains, new ThrowHandler()))
         {
-            
+            _componentStore = GetComponent<ComponentStore<CommandDomainComponent>>();
+        }
+
+        public bool IsDomainAllowed(CommandDomain domain)
+        {
+            // TODO: doing this for every command is a little cringe. We need a Contains() or something to find a specific key 
+            CommandDomainComponent[] commandDomainComponents = _componentStore.GetAllComponents();
+            foreach (CommandDomainComponent commandDomainComponent in commandDomainComponents)
+            {
+                if (domain == commandDomainComponent.AllowedCommandDomain)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
