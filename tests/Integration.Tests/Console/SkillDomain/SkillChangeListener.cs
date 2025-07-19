b03ae4b583e8ba74765aa7b03f19f0413fe986f5
meat <professionalkateg@gmail.com>
2025-07-19 21:24:04 +0100

    Created SkillDomainFlowTest and moved CurrencyDomainFlowTest into its own place

---
+++ tests/Integration.Tests/Console/SkillDomain/SkillChangeListener.cs
@@ -0,0 +1,18 @@
+﻿using IdelPog.Messaging.Listeners;
+using IdelPog.SimulationEngine.Skill;
+
+namespace Integration.Tests.Console
+{
+    public class SkillChangeListener : ISingleListener<SkillChange>
+    {
+        public Type ListenerType => typeof(SkillChange);
+        public bool WasCalled { get; private set; }
+        public SkillChange SkillChange { get; private set; }
+        
+        public void Handle(SkillChange item)
+        {
+            WasCalled = true;
+            SkillChange = item;
+        }
+    }
+}
---
+++ tests/Integration.Tests/Console/SkillDomain/SkillDomainFlowTest.cs
@@ -0,0 +1,21 @@
+﻿using Console;
+using Console.Runtime.Input;
+
+namespace Integration.Tests.Console
+{
+    [TestFixture]
+    public class SkillDomainFlowTest : ManagedBuffer
+    {
+        private IInputHandler _inputHandler;
+        private SkillChangeListener _skillChangeListener;
+
+        [SetUp]
+        public void Setup()
+        {
+            _inputHandler = ConsoleBootstrapper.Initialize(BufferManager);
+            
+            _skillChangeListener = new SkillChangeListener();
+            ManagedSubscribe(_skillChangeListener);
+        }
+    }
+}
