e3a8edfe6f10c859cb5a58242cc47c7eae6dae52
meat <professionalkateg@gmail.com>
2025-07-26 13:14:05 +0100

    Created ControlAction, which is used within ScheduleControl. Created the ScheduleControlListener for this

---
+++ Shared/Common/Commands/ScheduleControl.cs
@@ -0,0 +1,9 @@
+﻿using IdelPog.Common.Enums;
+
+namespace IdelPog.Common.Commands
+{
+    public readonly record struct ScheduleControl
+    {
+        public required ControlAction ControlAction { get; init; }
+    }
+}
