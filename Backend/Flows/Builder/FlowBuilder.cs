4a7d32c8b9c3228a087d69acd07b662ff0295f2d
meat <professionalkateg@gmail.com>
2025-07-29 20:30:25 +0100

    Created Flows. FlowBuilder is used to create FlowDescriptor. This will be used to create whole flows easier

---
+++ Backend/Flows/Builder/FlowBuilder.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace IdelPog.Flows.Builder
+{
+    public class FlowBuilder<TCommand>
+    {
+        public static FlowBuilder<TCommand> ForCommand => new();
+        
+        private Type? _controllerType;
+        private Type? _resultType;
+        private Type? _errorType;
+        private Type? _mediatorType;
+        
+        public FlowBuilder<TCommand> WithController<TController>()
+        {
+            _controllerType = typeof(TController);
+            return this;
+        }
+
+        public FlowBuilder<TCommand> WithMediator<TMediator>()
+        {
+            _mediatorType = typeof(TMediator);
+            return this;
+        }
+
+        public FlowBuilder<TCommand> OnSuccess<TResult>()
+        {
+            _resultType = typeof(TResult);
+            return this;
+        }
+
+        public FlowBuilder<TCommand> OnError<TError>()
+        {
+            _errorType = typeof(TError);
+            return this;
+        }
+
+        public FlowDescriptor Build()
+        {
+            Validate();
+            
+            return new FlowDescriptor
+            {
+                CommandType = typeof(TCommand),
+                ControllerType = _controllerType,
+                MediatorType = _mediatorType,
+                SuccessResultType = _resultType,
+                ErrorResultType = _errorType,
+            };
+        }
+
+        [MemberNotNull(nameof(_controllerType), nameof(_resultType), nameof(_errorType), nameof(_mediatorType))]
+        private void Validate()
+        {
+            Debug.Assert(_controllerType != null, nameof(_controllerType) + " != null");
+            Debug.Assert(_resultType != null, nameof(_resultType) + " != null");
+            Debug.Assert(_errorType != null, nameof(_errorType) + " != null");
+            Debug.Assert(_mediatorType != null, nameof(_mediatorType) + " != null");
+
+        }
+    }
+}
---
+++ Backend/Flows/Builder/FlowDescriptor.cs
@@ -0,0 +1,11 @@
+﻿namespace IdelPog.Flows.Builder
+{
+    public readonly record struct FlowDescriptor
+    {
+        public required Type CommandType { get; init; }
+        public required Type ControllerType { get; init; }
+        public required Type MediatorType { get; init; }
+        public required Type SuccessResultType { get; init; }
+        public required Type ErrorResultType { get; init; }
+    }
+}
