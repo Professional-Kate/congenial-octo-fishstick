4a60ee50891841753e205af8644ed7ad02ae02c6
meat <professionalkateg@gmail.com>
2025-08-20 20:34:53 +0100

    Removed the Content directory and project

--- Modules/Content/Hydrator/Assertion/Pipeline/IDirectoryAssertionPipeline.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IdelPog.Content.Hydrator.Assertion.Pipeline
-{
-    public interface IDirectoryAssertionPipeline
-    {
-        public void AssertDirectory(string directoryPath);
-
-        public void AssertFiles(int itemCount, string directoryPath);
-    }
-}
--- Modules/Content/Hydrator/GameContentContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json.Serialization;
-using IdelPog.Core.Contracts.Command;
-
-namespace IdelPog.Content.Hydrator
-{
-    [JsonSerializable(typeof(CurrencyCreation))]
-    public partial class GameContentContext : JsonSerializerContext;
-}
