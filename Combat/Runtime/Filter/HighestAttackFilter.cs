82b84998df4a3f54972da985f2195b5be24925f8
meat <professionalkateg@gmail.com>
2026-03-20 22:44:08 +0000

    Added HighestAttackFilter and tests

---
+++ Combat/Runtime/Filter/HighestAttackFilter.cs
@@ -0,0 +1,38 @@
+﻿using IdelPog.Combat.Runtime.Component;
+using IdelPog.Combat.Runtime.Filter.Interface;
+using IdelPog.Core.Validation.Assertion.Interface;
+
+namespace IdelPog.Combat.Runtime.Filter
+{
+    public sealed class HighestAttackFilter : ICombatantFilter
+    {
+        private readonly ICollectionAssertion _collectionAssertion;
+
+        public HighestAttackFilter(ICollectionAssertion collectionAssertion)
+        {
+            _collectionAssertion = collectionAssertion;
+        }
+
+        public byte GetEntity(CombatantEntity[] combatants)
+        {
+            _collectionAssertion.AssertHasElements(combatants);
+            
+            uint highestAttack = uint.MinValue;
+            byte combatantID = 0;
+            
+            foreach (CombatantEntity combatantEntity in combatants)
+            {
+                CombatantStatsComponent combatantStatsComponent = combatantEntity.GetComponent<CombatantStatsComponent>();
+                if (combatantStatsComponent.StatCard.Attack <= highestAttack)
+                {
+                    continue;
+                }
+
+                highestAttack = combatantStatsComponent.StatCard.Attack;
+                combatantID = combatantEntity.CombatantID;
+            }
+            
+            return combatantID;
+        }
+    }
+}
---
+++ Tests/Combat.Tests/Runtime/Filter/HighestAttackFilterTest.cs
@@ -0,0 +1,53 @@
+﻿using IdelPog.Combat.Contracts.Card;
+using IdelPog.Combat.Runtime;
+using IdelPog.Combat.Runtime.Filter;
+using IdelPog.Core.Repository.Asserter;
+using IdelPog.Core.Validation.Assertion;
+using IdelPog.Core.Validation.Exceptions;
+
+namespace IdelPog.Combat.Tests.Runtime.Filter
+{
+    [TestFixture]
+    public sealed class HighestAttackFilterTest
+    {
+        private HighestAttackFilter _highestAttackFilter;
+        private RepositoryAsserter _repositoryAsserter;
+        
+        private CombatantEntity _highAttackEntity;
+        private CombatantEntity _lowAttackEntity;
+        
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            _highestAttackFilter = new HighestAttackFilter(new CollectionAssertion());
+            _repositoryAsserter = new RepositoryAsserter(new FoundAssertion(), new ObjectNullAssertion(), new UniqueAssertion());
+
+            _highAttackEntity = new CombatantEntity(_repositoryAsserter, new StatCard { Attack = 8, Health = 7, Speed = 5 }) { IsFriendly = true, CombatantID = 12 };
+            _lowAttackEntity = new CombatantEntity(_repositoryAsserter, new StatCard { Attack = 2, Health = 6, Speed = 5 }) { IsFriendly = true, CombatantID = 27 };
+        }
+        
+        [Test]
+        public void Positive_GetEntity_FindsHighestAttackEntity()
+        {
+            byte combatantID = _highestAttackFilter.GetEntity([_lowAttackEntity, _lowAttackEntity, _highAttackEntity, _lowAttackEntity]);
+            
+            Assert.That(combatantID, Is.EqualTo(_highAttackEntity.CombatantID));
+        }
+        
+        [Test]
+        public void Positive_GetEntity_MaxAttack_ReturnsExpected()
+        {
+            CombatantEntity maxAttack = new(_repositoryAsserter, new StatCard { Attack = uint.MaxValue, Health = 1, Speed = 5 }) { IsFriendly = true, CombatantID = 25 };
+            
+            byte combatantID = _highestAttackFilter.GetEntity([_highAttackEntity, maxAttack, _highAttackEntity, _lowAttackEntity]);
+            
+            Assert.That(combatantID, Is.EqualTo(maxAttack.CombatantID));
+        }
+
+        [Test]
+        public void Negative_GetEntity_EmptyCollection_Throws()
+        { 
+            Assert.Throws<EmptyCollectionException>(() => _highestAttackFilter.GetEntity([]));
+        }
+    }
+}
